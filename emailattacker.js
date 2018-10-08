b07da66b9d3361dca77199b2cfd78f116dba3aa7
Oyeyemi Clement <barthclem@gmail.com>
2018-10-08 12:18:28 +0100

    Update emailattacker.js
    The block to select messages randomly

--- emailattacker.js
+++ emailattacker.js
@@ -18,8 +18,13 @@ var mailOptions = {
     subject:'subject of mail',
     text:'text data',
 };
+
+var randomMessages = ['have you ever been to New Orleaans?', 'I can not just listening to sad songs',
+                      'Have you read A Woman who runs with the Wolves', 'the Big bluf is not bad'];
 // this loop send 100 mail in a minute
 for(var i=0;i<100;i++){
+var randomMessageIndex = Math.floor(Math.random() * 4);
+var options = {...mailOptions, text: randomMessages[randomMessageIndex]};
 traspoter.sendMail(mailOptions,function(error, info){
     if (error){
         console.log(error);
